048708cddabcc3cca14dceaf78401a07a1272bb0
Ihor <i.radetskyi@batyevka.net>
2025-12-31 13:57:31 +0200

    commit

--- find.py
+++
@@ -1,7 +0,0 @@
-import sounddevice as sd
-
-print("=== LOOPBACK DEVICES ===")
-for i, d in enumerate(sd.query_devices()):
-    name = d["name"].lower()
-    if "loopback" in name or "wasapi" in name:
-        print(i, d)
